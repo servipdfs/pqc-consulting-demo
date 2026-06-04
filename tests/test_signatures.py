389d16d23abe4e0ce232149f225ba3be449c3abe
servipdfs <servipdfs@gmail.com>
2026-06-05 01:42:06 +0200

    Update test_signatures.py

--- tests/test_signatures.py
+++ tests/test_signatures.py
@@ -1,10 +1,9 @@
-import pytest
 import sys
 sys.path.append('src')
-from signature_demo import generate_dilithium_keypair, sign_message, verify_signature
+from signature_demo import generate_keypair, sign, verify
 
-def test_dilithium_signature():
-    pub, priv = generate_dilithium_keypair()
-    msg = b"Important transaction data"
-    sig = sign_message(priv, msg)
-    assert verify_signature(pub, msg, sig) == True
+def test_sign():
+    pub, priv = generate_keypair()
+    msg = b"test"
+    sig = sign(priv, msg)
+    assert verify(pub, msg, sig) is True
